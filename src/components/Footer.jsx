fa139301b603d316a2a8231ba8f793f4e0ee5560
Aivz Villanueva <aivz.villanueva@gmail.com>
2024-04-17 10:39:55 -0600

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
   <div name='credits' className='w-ful bg-[#0a192f] flex justify-left items-left'>
         <div className='flex flex-col max-w-[600px] w-full'>
-          <p className=''><a href='https://buttercms.com'><img src='/butter-b.png' width='200'/></a></p>
+          <p className=''><a href='https://buttercms.com'><img src='/butter-w.png' width='200'/></a></p>
             <p className='inline text-gray-300'><a href="https://www.flaticon.com/free-icons/" title="icons">Programming Icons created by Freepik - Flaticon</a></p>
         </div>
 </div>
